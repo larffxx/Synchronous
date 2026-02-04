0a644b72c7dce998582711675d1e948a5c5ec1f3
Демидов Яков Константинович <officiallarffxx@hotmail.com>
2026-02-04 14:19:24 +0300

    Create a single ExceptionHandler

---
+++ SynchronousDiscord/src/main/java/com/larffxx/synchronousdiscord/dto/ErrorResponse.java
@@ -0,0 +1,6 @@
+package com.larffxx.synchronousdiscord.dto;
+
+public record ErrorResponse(
+    String code,
+    String message
+) {}
--- SynchronousDiscord/src/main/java/com/larffxx/synchronousdiscord/infexc/InfExcMessages.java
+++ SynchronousDiscord/src/main/java/com/larffxx/synchronousdiscord/infexc/InfExcMessages.java
@@ -1,9 +1,17 @@
 package com.larffxx.synchronousdiscord.infexc;
 
 public class InfExcMessages {
+    public static final String SYNCHRONOUS_DISCORD_EXCEPTION = "SynchronousDiscordException";
     public static final String DOWNLOAD_ATTACHMENT_EXCEPTION = "Download attachment exception";
     public static final String VALUES_NOT_PROVIDED_ERROR = "No values have been provided";
     public static final String REQUIRED_VALUE_NOT_PROVIDED = "Required value is not provided";
     public static final String NO_REGISTERED_USERS = "No registered users has been provided";
     public static final String SERVER_IS_NOT_CONNECTED_TO_TELEGRAM = "Server is not connected to the telegram";
+    public static final String ROUTE_SERVICE_EXCEPTION = "Route service exception";
+    public static final String VERIFY_EXCEPTION = "Verify exception";
+    public static final String DISCORD_SLASH_INTERACTION_EXCEPTION = "Discord Slash Interaction exception";
+    public static final String TELEGRAM_SLASH_INTERACTION_EXCEPTION = "Telegram Slash Interaction exception";
+    public static final String COMMAND_EXCEPTION = "Command exception";
+    public static final String DELETE_MESSAGE_EXCEPTION = "Delete message exception";
+    public static final String SHUFFLE_EXCEPTION = "Shuffle exception";
 }
