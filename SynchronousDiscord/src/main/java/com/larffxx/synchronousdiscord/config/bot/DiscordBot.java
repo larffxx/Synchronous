80c896f821bcddac4b1694bb34906222f2eba1e4
Демидов Яков Константинович <officiallarffxx@hotmail.com>
2026-04-22 21:06:38 +0300

    Add DAVE support for audio connection

--- SynchronousDiscord/src/main/java/com/larffxx/synchronousdiscord/config/bot/DiscordBot.java
+++ SynchronousDiscord/src/main/java/com/larffxx/synchronousdiscord/config/bot/DiscordBot.java
@@ -5,8 +5,13 @@ import com.larffxx.synchronousdiscord.receiver.EventReceiver;
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.Setter;
+import moe.kyokobot.libdave.DaveFactory;
+import moe.kyokobot.libdave.NativeDaveFactory;
+import moe.kyokobot.libdave.jda.LDJDADaveSessionFactory;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.audio.AudioModuleConfig;
+import net.dv8tion.jda.api.audio.dave.DaveSessionFactory;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import org.springframework.beans.factory.annotation.Value;
@@ -29,9 +34,14 @@ public class DiscordBot {
 
     @PostConstruct
     public void settingUpDiscord() {
+        DaveFactory daveFactory = new NativeDaveFactory();
+        DaveSessionFactory daveSessionFactory = new LDJDADaveSessionFactory(daveFactory);
+
         jda = JDABuilder.create(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_VOICE_STATES, GatewayIntent.GUILD_MEMBERS, GatewayIntent.MESSAGE_CONTENT).enableCache(CacheFlag.VOICE_STATE)
                 .addEventListeners(eventsListener)
                 .enableIntents(GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_EXPRESSIONS, GatewayIntent.SCHEDULED_EVENTS)
+                .setAudioModuleConfig(new AudioModuleConfig()
+                        .withDaveSessionFactory(daveSessionFactory))
                 .build();
         eventReceiver.setJda(jda);
     }
